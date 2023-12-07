699d1947ee0ec700c7ae266946926c34cf3d12e8
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-07 21:08:41 +0200

    form-wave-animation

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const MINIPROJECTS = [
-  "01_expanding_cards",
-  "02_progress_steps",
-  "03_rotating_navigation",
-  "04_hidden_search",
-  "05_blurry_loading",
-  "06_scroll_animation",
-  "07_split_landing_page",
+    "01_expanding_cards",
+    "02_progress_steps",
+    "03_rotating_navigation",
+    "04_hidden_search",
+    "05_blurry_loading",
+    "06_scroll_animation",
+    "07_split_landing_page",
+    "08_form_wave_animation",
 ];
 
 const markup = MINIPROJECTS.map(element => {
-  return `<li><a href="./${element}/index.html">${element}</a></li>`;
+    return `<li><a href="./${element}/index.html">${element}</a></li>`;
 }).join("");
 
 document.querySelector(".main").innerHTML = `<ul>${markup}</ul>`;
